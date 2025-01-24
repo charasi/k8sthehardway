e63dd4cdf861f244811e032e2af3c6ba77b47c06
charasi <casiama@andrew.cmu.edu>
2025-01-24 16:29:04 -0600

    added new directories

--- ic/bucket/buckets.tf
+++ ic/bucket/buckets.tf
@@ -110,6 +110,14 @@ resource "google_storage_bucket_object" "etcd_client_csr" {
 }
 
 # Upload a file to the GCS bucket
+resource "google_storage_bucket_object" "reverse_proxy_csr" {
+  bucket = google_storage_bucket.kthw_misc.name
+  name   = "reverse-proxy-csr.json"
+  source = "../certificates/reverse-proxy-csr.json"
+  content_type = "application/json"
+}
+
+# Upload a file to the GCS bucket
 resource "google_storage_bucket_object" "reverse_proxy" {
   bucket = google_storage_bucket.kthw_misc.name
   name   = "nginx.conf"
