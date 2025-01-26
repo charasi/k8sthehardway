61d3e39cffeac751e3fb980dba8039e9846b95dd
charasi <casiama@andrew.cmu.edu>
2025-01-25 22:02:14 -0600

    modified

--- ic/worker_script.sh
+++ ic/worker_script.sh
@@ -18,19 +18,5 @@ sudo apt-get install -y ansible-core
 # install nginx
 sudo apt install nginx -y
 
-sudo mkdir -p /etc/cni/net.d
-sudo mkdir -p /opt/cni/bin
-sudo mkdir -p /var/lib/kubelet
-sudo mkdir -p /var/lib/kube-proxy
-sudo mkdir -p /var/lib/kubernetes
-sudo mkdir -p /var/run/kubernetes
-
-sudo chown root:root /etc/cni/net.d
-sudo chown root:root /opt/cni/bin
-sudo chown root:root /var/lib/kubelet
-sudo chown root:root /var/lib/kube-proxy
-sudo chown root:root /var/lib/kubernetes
-sudo chown root:root /var/run/kubernetes
-
 # Script completion message
 echo "All required packages (Maven, Java, Python3, Ansible) have been installed successfully."
